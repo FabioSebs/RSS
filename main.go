5f5d6e6df65106eafe1cf3cef50e52443262b14a
FabioSebs <sebsphotosofficial@gmail.com>
2024-06-10 21:52:44 +0700

    server

--- main.go
+++ main.go
@@ -1,32 +1,41 @@
 package main
 
-import "github.com/FabioSebs/RSS/scraper"
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/FabioSebs/RSS/scheduler/jobs"
+	"github.com/FabioSebs/RSS/server"
+	"github.com/labstack/gommon/color"
+)
 
 func main() {
-	// ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())
 
 	// go routine for listening for os signal (os.Interrupt)
-	// go func() {
-	// 	signals := make(chan os.Signal, 1)
-	// 	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
-	// 	<-signals // thread hangs until signal is found and written
-	// 	cancel()  // sends message to ctx
-	// }()
+	go func() {
+		signals := make(chan os.Signal, 1)
+		signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+		<-signals // thread hangs until signal is found and written
+		cancel()  // sends message to ctx
+	}()
 
 	// start server in seperate thread
-	// go func() {
-	// 	color.Println(color.Green("⇨ server up and running "))
-	// 	server.RunServer()
-	// }()
+	go func() {
+		color.Println(color.Green("⇨ server up and running "))
+		server.RunServer()
+	}()
 
 	// start scheduler in seperate thread
-	// go jobs.RunScraper()
+	go jobs.RunScraper()
 
-	// any other process
-	scraper := scraper.NewVietnamScraper()
-	scraper.LaunchScraper(scraper.CollectorSetup())
+	// // any other process
+	// scraper := scraper.NewVietnamScraper()
+	// scraper.LaunchScraper(scraper.CollectorSetup())
 
 	// main thread is waiting for os interrupt aka context cancel
-	// <-ctx.Done()
+	<-ctx.Done()
 
 }
